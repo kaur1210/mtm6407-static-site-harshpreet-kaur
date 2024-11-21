cecddff954ec2f73dceb484b8c4be0e04d8fd31d
harshpreet kaur <133127233+kaur1210@users.noreply.github.com>
2024-11-21 05:09:50 +0500

    code

---
+++ _nuxt/static/1732147542/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmtm6407-static-site-harshpreet-kaur\u002F_nuxt\u002Fstatic\u002F1732147542",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmtm6407-static-site-harshpreet-kaur\u002F",assetsPath:"\u002Fmtm6407-static-site-harshpreet-kaur\u002F_nuxt\u002F",cdnURL:a}}}}(null));
